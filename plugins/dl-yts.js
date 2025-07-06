de8b6a6441edd2e864d0d47327cb2d53860b82c8
Bajobots1-1 <kleinergalindo4@gmail.com>
2025-07-06 01:07:17 -0500

    Update dl-yts.js

--- plugins/dl-yts.js
+++ plugins/dl-yts.js
@@ -1,19 +1,29 @@
-import yts from 'yt-search'
-let handler = async (m, {conn, text }) => {
-  if (!text) throw `✳️ $Ingresa el título de un vídeo de YouTube.`
-  let results = await yts(text)
-let tes = results.videos
-let ms = tes.map(v => `
-° ${v.title}
+//By Bajo Bots
 
-≡ 🌳 \`Duración :\` ${v.timestamp}
-≡ 🌴 \`Subido :\` ${v.ago}
-≡ 🍁 \`Vistas :\` ${v.views.toLocaleString()}
-≡ 🌿 \`Link :\` ${v.url}
-`.trim()).join('\n________________________\n\n')
-let teks = `\`\`\`乂 YOUTUBE - SEARCH\`\`\`\n\n${ms}`
-conn.sendFile(m.chat, tes[0].image, 'yts.jpeg', teks, m)
+import yts from 'yt-search'
+
+let handler = async (m, { conn, text }) => {
+  if (!text) throw `🚫 𝙄𝙣𝙜𝙧𝙚𝙨𝙖 𝙚𝙡 𝙣𝙤𝙢𝙗𝙧𝙚 𝙙𝙚 𝙪𝙣 𝙫𝙞𝙙𝙚𝙤 𝙙𝙚 𝙔𝙤𝙪𝙏𝙪𝙗𝙚.\n\n📌 *Ejemplo:* .ytsearch Bizarrap Shakira`
+
+  let results = await yts(text)
+  let videos = results.videos
+
+  let info = videos.map(v => `
+┏━━━━━━━━━━━━━━━━━━━⬣
+┃✨ *${v.title}*
+┣━━━━━━━━━━━━━━━━━━━✦
+┃⏳ *Duración:* ${v.timestamp}
+┃📅 *Subido:* ${v.ago}
+┃👀 *Vistas:* ${v.views.toLocaleString()}
+┃🔗 *Enlace:* ${v.url}
+┗━━━━━━━━━━━━━━━━━━━⬣
+`).join('\n')
+
+  let message = `┏━━━━━━━━━━━━━━⬣\n┃🎬 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 - 𝐁𝐔𝐒𝐐𝐔𝐄𝐃𝐀\n┗━━━━━━━━━━━━━━⬣\n\n${info}`
+
+  conn.sendFile(m.chat, videos[0].image, 'yts.jpeg', message, m)
 }
+
 handler.help = ['ytsearch'] 
 handler.tags = ['download']
 handler.command = ['ytsearch', 'yts'] 
