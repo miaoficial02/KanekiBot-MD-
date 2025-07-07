c6115775bf6c71ec55605c6376e27c664f3b8fb2
Bajobots1-1 <kleinergalindo4@gmail.com>
2025-07-07 16:09:01 -0500

    Update dl-facebook.js

--- plugins/dl-facebook.js
+++ plugins/dl-facebook.js
@@ -2,7 +2,7 @@ import { igdl } from 'ruhend-scraper'
 
 const handler = async (m, { text, conn, args }) => {
   if (!args[0]) {
-    return conn.reply(m.chat, `${emoji} Por favor, ingresa un enlace de Facebook.`)
+    return conn.reply(m.chat, ` Por favor, ingresa un enlace de Facebook.`)
   }
 
   let res;
@@ -10,23 +10,23 @@ const handler = async (m, { text, conn, args }) => {
     await m.react(rwait);
     res = await igdl(args[0]);
   } catch (e) {
-    return conn.reply(m.chat, `${msm} Error al obtener datos. Verifica el enlace.`)
+    return conn.reply(m.chat, ` Error al obtener datos. Verifica el enlace.`)
   }
 
   let result = res.data;
   if (!result || result.length === 0) {
-    return conn.reply(m.chat, `${emoji2} No se encontraron resultados.`)
+    return conn.reply(m.chat, ` No se encontraron resultados.`)
   }
 
   let data;
   try {
     data = result.find(i => i.resolution === "720p (HD)") || result.find(i => i.resolution === "360p (SD)");
   } catch (e) {
-    return conn.reply(m.chat, `${msm} Error al procesar los datos.`)
+    return conn.reply(m.chat, ` Error al procesar los datos.`)
   }
 
   if (!data) {
-    return conn.reply(m.chat, `${emoji2} No se encontró una resolución adecuada.`)
+    return conn.reply(m.chat, `No se encontró una resolución adecuada.`)
   }
 
   let video = data.url;
@@ -34,7 +34,7 @@ const handler = async (m, { text, conn, args }) => {
     await conn.sendMessage(m.chat, { video: { url: video }, caption: `${emoji} Aqui tienes ฅ^•ﻌ•^ฅ.`, fileName: 'fb.mp4', mimetype: 'video/mp4' }, { quoted: m })
     await m.react(done);
   } catch (e) {
-    return conn.reply(m.chat, `${msm} Error al enviar el video.`)
+    return conn.reply(m.chat, ` Error al enviar el video.`)
     await m.react(error);
   }
 }
