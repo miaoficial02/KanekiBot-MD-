8b9f6ebe3f56f167f286387d2a7c7c13db3a7056
Bajobots1-1 <kleinergalindo4@gmail.com>
2025-07-07 01:56:05 -0500

    Update test-miniatura.js

--- plugins/test-miniatura.js
+++ plugins/test-miniatura.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-  let texto = `✨ *MeliodasBot-MD está activo*\nEste mensaje solo tiene miniatura.`
+  let texto = `✨ *KanekiBot-MD está activo*\nEste mensaje solo tiene miniatura.`
 
   await conn.sendMessage(m.chat, {
     text: texto,
@@ -7,7 +7,7 @@ let handler = async (m, { conn }) => {
       externalAdReply: {
         title: '\u200e', // carácter invisible
         body: '\u200e',  // carácter invisible
-        thumbnailUrl: 'https://qu.ax/bjOsy.jpg',
+        thumbnailUrl: 'https://qu.ax/VGCPX.jpg',
         mediaType: 1,
         renderLargerThumbnail: true,
         showAdAttribution: false,
@@ -17,5 +17,5 @@ let handler = async (m, { conn }) => {
   }, { quoted: m })
 }
 
-handler.command = /^testminiatura$/i
+handler.command = /^testminiatura$/i ,/^prueba$/i
 export default handler
