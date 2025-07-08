11925613e4849b691de908c281f694a79df7d82a
Bajobots1-1 <kleinergalindo4@gmail.com>
2025-07-08 11:12:22 -0500

    Update dl-play2.js

--- plugins/dl-play2.js
+++ plugins/dl-play2.js
@@ -71,5 +71,5 @@ const handler = async (m, { conn, args, command, usedPrefix }) => {
 
 handler.command = /^play2$/i;
 handler.tags = ['descargar'];
-//handler.help = ['play <nombre de canción/artista>'];
+handler.help = [''];
 export default handler;
