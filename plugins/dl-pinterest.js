cd3da0ea5b9d6d2d85dc073129f9d62aa90b47d2
Bajobots1-1 <kleinergalindo4@gmail.com>
2025-07-08 12:02:31 -0500

    Update dl-pinterest.js

--- plugins/dl-pinterest.js
+++ plugins/dl-pinterest.js
@@ -67,7 +67,7 @@ let handler = async (m, { conn, text, args }) => {
   }
 };
 
-handler.help = [''];
+handler.help = ['pinterest <texto o link>'];
 handler.command = ['pinterest', 'pin'];
 handler.tags = ["descargar"];
 export default handler;
