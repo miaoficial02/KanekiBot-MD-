f2996617b77d82f87ff7d753ca15c7c8be0cde19
Bajobots1-1 <kleinergalindo4@gmail.com>
2025-07-08 12:29:02 -0500

    Update dl-pinterest.js

--- plugins/dl-pinterest.js
+++ plugins/dl-pinterest.js
@@ -119,6 +119,6 @@ let handler = async (m, { conn, text }) => {
 handler.help = ['pinterest'];
 handler.command = ['pinterest', 'pin'];
 handler.tags = ['buscador'];
-handler.register = true
+handler.register = false
 
 export default handler;
