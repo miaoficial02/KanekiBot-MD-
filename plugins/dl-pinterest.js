ad72b1d1854a710d39d297e7d938f93049657ac9
Bajobots1-1 <kleinergalindo4@gmail.com>
2025-07-08 12:30:03 -0500

    Update dl-pinterest.js

--- plugins/dl-pinterest.js
+++ plugins/dl-pinterest.js
@@ -86,7 +86,7 @@ const pins = async (judul) => {
 };
 
 let handler = async (m, { conn, text }) => {
-  if (!text) return conn.reply(m.chat, `${emojis} Ingresa un texto. Ejemplo: .pinterest ${botname}`, m, fake);
+  if (!text) return conn.reply(m.chat, ` Ingresa un texto. Ejemplo: .pinterest ${botname}`, m, fake);
   
 
   try {
