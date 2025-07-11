4611227abd3e0aa98076926a4058dcebacc583ea
Bajobots1-1 <kleinergalindo4@gmail.com>
2025-07-11 16:44:00 -0500

    Update tools-tourl2.js

--- plugins/tools-tourl2.js
+++ plugins/tools-tourl2.js
@@ -5,7 +5,7 @@ import fetch from 'node-fetch'
 let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) return conn.reply(m.chat, ` Por favor, responda a una *Imagen* o *Vídeo.*`, m)
+  if (!mime) return conn.reply(m.chat, ` Por favor, responda a una *Imagen* o *Vídeo.*`)
   await m.react(rwait)
   try {
   let media = await q.download()
