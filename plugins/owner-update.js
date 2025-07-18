6ae78bd32ada364fa96e94c623645ae20f64a102
Bajobots1-1 <kleinergalindo4@gmail.com>
2025-07-18 18:51:02 -0500

    Update owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -10,7 +10,7 @@ conn.reply(m.chat, stdout.toString(), m)
 }}
 handler.help = ['update']
 handler.tags = ['owner']
-handler.command = ['update', 'actualizar', 'fix', 'fixed'] 
+handler.command = ['update', 'actualizar', 'fix', 'fixed', 'up'] 
 handler.rowner = true
 
 export default handler
