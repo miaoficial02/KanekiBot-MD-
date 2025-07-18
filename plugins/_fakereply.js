8ef785a5e002250385121e65948206d728fd0754
Wichi <sebastianbarbaro82@gmail.com>
2025-07-18 12:11:46 -0400

    Crear _fakereply.js

---
+++ plugins/_fakereply.js
@@ -0,0 +1,71 @@
+import fetch from 'node-fetch'
+
+export async function before(m, { conn }) {
+let name = `ᥴһᥲᥒᥒᥱᥣ-sᥲsᥙkᥱ ᑲ᥆𝗍 mძ 🌀`
+let imagenes = ["https://files.catbox.moe/6dewf4.jpg",
+"https://files.catbox.moe/6dewf4.jpg",
+"https://files.catbox.moe/6dewf4.jpg",
+"https://files.catbox.moe/6dewf4.jpg",]
+
+let icono = imagenes[Math.floor(Math.random() * imagenes.length)]
+
+global.rcanal = {
+ contextInfo: {
+             isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: "120363419364337473@newsletter",
+      serverMessageId: 100,
+      newsletterName: name,
+   }, 
+   externalAdReply: {
+    showAdAttribution: true, 
+    title: botname, 
+    body: textbot, 
+    mediaUrl: null, 
+    description: null, 
+    previewType: "PHOTO", 
+    thumbnailUrl: icono, 
+    sourceUrl: canal, 
+    mediaType: 1, 
+    renderLargerThumbnail: false }, 
+    }, 
+    }
+
+
+global.icono = [ 
+'https://qu.ax/yyCo.jpeg',
+'https://qu.ax/yyCo.jpeg',
+'https://qu.ax/qJch.jpeg',
+'https://qu.ax/qJch.jpeg',
+'https://qu.ax/CHRS.jpeg',
+'https://qu.ax/CHRS.jpeg',
+].getRandom()
+
+        global.fkontak = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wm, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': fs.readFileSync('./storage/img/catalogo.png'), thumbnail: fs.readFileSync('./storage/img/catalogo.png'), sendEphemeral: true } } }
+
+  // Respuesta con enlace de WhatsApp
+  global.rpl = {
+    contextInfo: {
+      externalAdReply: {
+        mediaUrl: group,
+        mediaType: 'VIDEO',
+        description: 'support group',
+        title: packname,
+        body: 'grupo de soporte',
+        thumbnailUrl: 'imagen2',
+        sourceUrl: group,
+      }
+    }
+  };
+
+ global.fake = {
+    contextInfo: {
+            isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: "120363419364337473@newsletter",
+      serverMessageId: 100,
+      newsletterName: name,
+    },
+    },
+  }
+}
