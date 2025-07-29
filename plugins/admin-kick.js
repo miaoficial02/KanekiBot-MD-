bf2774af6ecf658010427e884070af8ffa7786d6
Bajobots1-1 <kleinergalindo4@gmail.com>
2025-07-28 23:57:22 -0500

    Update admin-kick.js

--- plugins/admin-kick.js
+++ plugins/admin-kick.js
@@ -25,8 +25,34 @@ let handler = async (m, { conn, usedPrefix, command }) => {
       }
     }
   }, { quoted: m })
+const fkontak = {
+    key: {
+      participants: "0@s.whatsapp.net",
+      remoteJid: "status@broadcast",
+      fromMe: false,
+      id: "Halo"
+    },
+    message: {
+      locationMessage: {
+        name: "𝖬𝖤𝖭𝖴 𝖢𝖮𝖬𝖯𝖫𝖤𝖳𝖮 👾",
+        jpegThumbnail: await (await fetch('https://iili.io/F8Y2bS9.jpg')).buffer(),
+        vcard:
+          "BEGIN:VCARD\n" +
+          "VERSION:3.0\n" +
+          "N:;Unlimited;;;\n" +
+          "FN:Unlimited\n" +
+          "ORG:Unlimited\n" +
+          "TITLE:\n" +
+          "item1.TEL;waid=19709001746:+1 (970) 900-1746\n" +
+          "item1.X-ABLabel:Unlimited\n" +
+          "X-WA-BIZ-DESCRIPTION:ofc\n" +
+          "X-WA-BIZ-NAME:Unlimited\n" +
+          "END:VCARD"
+      }
+    },
+    participant: "0@s.whatsapp.net"
+  };
 
-}
 
 handler.help = ['kick @user']
 handler.tags = ['group']
