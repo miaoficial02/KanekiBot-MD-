d8555c1eb3cd0fa08c2c5a7467f68927da4c7c33
Bajobots1-1 <kleinergalindo4@gmail.com>
2025-07-30 17:12:05 -0500

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,19 +1,19 @@
 let handler = async (m, { args, usedPrefix, command }) => {
   const name = args.join(" ")
-  if (!name) return m.reply(`✨ *Usa el comando así:*\n\n${usedPrefix + command} MiBotPersonal`)
+  if (!name) return m.reply(`✨ *Usa el comando así:*\n\n${usedPrefix + command} MiSubBot`)
 
   if (name.length > 25) return m.reply("⚠️ *El nombre es muy largo.* Usa menos de 25 caracteres.")
 
   global.db.data.settings = global.db.data.settings || {}
   global.db.data.settings[m.sender] = global.db.data.settings[m.sender] || {}
-  global.db.data.settings[m.sender].menuBotName = name
+  global.db.data.settings[m.sender].menuBotName = name // SOLO para ese subbot
 
-  m.reply(`✅ *Nombre del menú actualizado a:* *${name}*\n\n📌 Usa *.menu* para ver el cambio.`)
+  m.reply(`✅ *Nombre del menú de tu subbot cambiado a:* *${name}*`)
 }
- 
+
 handler.help = ['setname <nombre>']
 handler.tags = ['']
 handler.command = /^setname$/i
-handler.register = false
+handler.register = true
 
 export default handler
