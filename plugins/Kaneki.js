b7e0ed4b1c6a3af5150eaceafe34e6d44aaadbd6
Bajobots1-1 <kleinergalindo4@gmail.com>
2025-08-04 15:21:54 -0500

    Update Kaneki.js

--- plugins/Kaneki.js
+++ plugins/Kaneki.js
@@ -1,8 +1,7 @@
 import fetch from 'node-fetch';
 
 const handler = async (m, { conn }) => {
-  const videoUrl = 'https://files.catbox.moe/vx01o1.jpg'; 
-  const thumb = await (await fetch('https://iili.io/F8Y2bS9.jpg')).buffer();
+  const videoUrl = 'https://files.catbox.moe/vx01o1.jpg';
 
   const caption = `
 🎥 *KANEKI INFERNAL* 👁‍🗨
@@ -14,28 +13,43 @@ const handler = async (m, { conn }) => {
 
   const fkontak = {
     key: {
-      participants: "0@s.whatsapp.net",
-      remoteJid: "status@broadcast",
+      participants: '0@s.whatsapp.net',
+      remoteJid: 'status@broadcast',
       fromMe: false,
-      id: "Kaneki"
+      id: 'Halo'
     },
     message: {
       locationMessage: {
-        name: "Video especial: Kaneki 👾",
-        jpegThumbnail: thumb
+        name: 'KANEKI DIOS',
+        vcard:
+          'BEGIN:VCARD\n' +
+          'VERSION:3.0\n' +
+          'N:;Unlimited;;;\n' +
+          'FN:Unlimited\n' +
+          'ORG:Unlimited\n' +
+          'TITLE:\n' +
+          'item1.TEL;waid=19709001746:+1 (970) 900-1746\n' +
+          'item1.X-ABLabel:Unlimited\n' +
+          'X-WA-BIZ-DESCRIPTION:ofc\n' +
+          'X-WA-BIZ-NAME:Unlimited\n' +
+          'END:VCARD'
       }
-    }
+    },
+    participant: '0@s.whatsapp.net'
   };
 
   await conn.sendMessage(m.chat, {
     video: { url: videoUrl },
     gifPlayback: true,
     caption: caption
-  }, { quoted: fkontak });
+  }, {
+    quoted: fkontak
+  });
 };
 
-handler.customPrefix = /^kaneki$/i; 
-handler.command = new RegExp(); 
+
+handler.customPrefix = /^kaneki$/i;
+handler.command = new RegExp();
 handler.before = true;
 
 export default handler;
